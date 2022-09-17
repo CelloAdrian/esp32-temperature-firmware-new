24971d352c02eeef8b0d32fb9ce9584f58a004af
lasergangers <celloadrian29@Protonmail.com>
2022-09-17 08:52:21 +0700

    feat: implement switching to STA after receiving STA credentials

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #define AP_PORT 80
 #define AP_SSID "ESP32-AP"
 #define AP_PW "123456789"
-#define DHT_TYPE DHT21;
+#define DHT_TYPE DHT21
 
 const int DHT_PIN = 13;
 
@@ -26,8 +26,8 @@ void setup() {
 }
 
 String header;
-// String STA_SSID;
-// String STA_PW;
+char* STA_SSID;
+char* STA_PW;
 
 void loop() {
     WiFiClient client = server.available();
@@ -54,7 +54,7 @@ void loop() {
                             DeserializationError error = deserializeJson(doc, header);
 
                             deserializeJson(doc, header);
-                            const char* STA_SSID = doc["sta_ssid"];
+                            STA_SSID = doc["sta_ssid"];
                             Serial.println("STA_SSID:");
                             Serial.println(STA_SSID);
                         }
@@ -62,6 +62,28 @@ void loop() {
                         if (header.indexOf("GET /sta_pw") >= 0) {
                             // client.print("HTTP/1.1 200 OK\nContent-type: text/html\nConnection: close\n\n");
                             // client.print("<!DOCTYPE html><html><body><h1>STA_PW</h1></body></html>\n");
+                            StaticJsonDocument<200> doc;
+
+                            DeserializationError error = deserializeJson(doc, header);
+
+                            deserializeJson(doc, header);
+                            STA_PW = doc["sta_pw"];
+                            Serial.println("STA_PW:");
+                            Serial.println(STA_PW);
+                        }
+
+                        if (STA_SSID || STA_PW != "") {
+                            WiFi.mode(WIFI_STA);
+                            WiFi.begin(STA_SSID, STA_PW);
+                            Serial.println("Connecting to WiFi..");
+                            while (WiFi.status() != WL_CONNECTED) {
+                                delay(500);
+                                Serial.print(".");
+                            }
+                            Serial.println("");
+                            Serial.println("Connected to STA WiFi");
+                            Serial.println("IP address: ");
+                            Serial.println(WiFi.localIP());
                         }
                         break;
                     } else {
@@ -72,6 +94,23 @@ void loop() {
                 }
             }
         }
+
+        if (WiFi.getMode() == WIFI_STA) {
+            if (WiFi.status() == WL_CONNECTED) {
+                if (client.connected()) {
+                    // read sensor data
+                    DHT dht(DHT_PIN, DHT_TYPE);
+                    float h = dht.readHumidity();
+                    float t = dht.readTemperature();
+                    // check if any reads failed and exit early, then try again
+                    if (isnan(h) || isnan(t)) {
+                        Serial.println("Failed to read from DHT sensor!");
+                        return;
+                    }
+                }
+            }
+        }
+
         header = "";
         client.stop();
         Serial.println("Client disconnected");
