4d23fac0fd9f38eac8dce99efa7fed73abd60a89
lasergangers <celloadrian29@Protonmail.com>
2022-09-20 09:28:28 +0700

    refactor: rewrite STA mode logic

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 const int DHT_PIN = 13;
 
 WiFiServer server(AP_PORT);
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
     Serial.begin(BAUD_RATE);
@@ -26,93 +27,97 @@ void setup() {
 }
 
 String header;
-char* STA_SSID;
-char* STA_PW;
+const char* STA_SSID;
+const char* STA_PW;
 
 void loop() {
     WiFiClient client = server.available();
-    if (client) {
-        Serial.println("new client");
-        std::string currentLine = "";
-        while (client.connected()) {
-            if (client.available()) {
-                char c = client.read();
-                Serial.write(c);
-                header += c;
-                if (c == '\n') {
-                    if (currentLine.length() == 0) {
-                        // respond OK so request wont hang
-                        client.print("HTTP/1.1 200 OK\nContent-type: text/html\nConnection: close\n\n");
-                        client.print("<!DOCTYPE html><html><body><h1>HI FROM ESP32</h1></body></html>\n");
 
-                        // test routes
-                        if (header.indexOf("GET /sta_ssid") >= 0) {
-                            // client.print("HTTP/1.1 200 OK\nContent-type: text/html\nConnection: close\n\n");
-                            // client.print("<!DOCTYPE html><html><body><h1>STA_SSID</h1></body></html>\n");
-                            StaticJsonDocument<200> doc;
-                            // deserialize the incoming post request
-                            DeserializationError error = deserializeJson(doc, header);
+    if (WiFi.getMode() == WIFI_AP) {
+        if (client) {
+            Serial.println("new client");
+            String currentLine = "";
+            while (client.connected()) {
+                if (client.available()) {
+                    char c = client.read();
+                    Serial.write(c);
+                    header += c;
+                    if (c == '\n') {
+                        if (currentLine.length() == 0) {
+                            // respond OK so request wont hang
+                            client.print("HTTP/1.1 200 OK\nContent-type: text/html\nConnection: close\n\n");
+                            client.print("<!DOCTYPE html><html><body><h1>ESP32 Webserver</h1></body></html>\n");
 
-                            deserializeJson(doc, header);
-                            STA_SSID = doc["sta_ssid"];
-                            Serial.println("STA_SSID:");
-                            Serial.println(STA_SSID);
-                        }
+                            // test routes
+                            if (header.indexOf("GET /sta_ssid") >= 0) {
+                                StaticJsonDocument<200> doc;
+                                DeserializationError error = deserializeJson(doc, header);
 
-                        if (header.indexOf("GET /sta_pw") >= 0) {
-                            // client.print("HTTP/1.1 200 OK\nContent-type: text/html\nConnection: close\n\n");
-                            // client.print("<!DOCTYPE html><html><body><h1>STA_PW</h1></body></html>\n");
-                            StaticJsonDocument<200> doc;
-
-                            DeserializationError error = deserializeJson(doc, header);
-
-                            deserializeJson(doc, header);
-                            STA_PW = doc["sta_pw"];
-                            Serial.println("STA_PW:");
-                            Serial.println(STA_PW);
-                        }
-
-                        if (STA_SSID || STA_PW != "") {
-                            WiFi.mode(WIFI_STA);
-                            WiFi.begin(STA_SSID, STA_PW);
-                            Serial.println("Connecting to WiFi..");
-                            while (WiFi.status() != WL_CONNECTED) {
-                                delay(500);
-                                Serial.print(".");
+                                deserializeJson(doc, header);
+                                STA_SSID = doc["sta_ssid"];
+                                // test
+                                Serial.println("STA_SSID: ");
+                                Serial.println(STA_SSID);
+                                // test
                             }
-                            Serial.println("");
-                            Serial.println("Connected to STA WiFi");
-                            Serial.println("IP address: ");
-                            Serial.println(WiFi.localIP());
+
+                            if (header.indexOf("GET /sta_pw") >= 0) {
+                                StaticJsonDocument<200> doc;
+                                DeserializationError error = deserializeJson(doc, header);
+
+                                deserializeJson(doc, header);
+                                STA_PW = doc["sta_pw"];
+                                // test
+                                Serial.println("STA_PW: ");
+                                Serial.println(STA_PW);
+                                // test
+                            }
+                            // test routes
+
+                            if (STA_SSID != NULL && STA_PW != NULL) {
+                                WiFi.mode(WIFI_STA);
+                            }
                         }
-                        break;
-                    } else {
-                        currentLine = "";
-                    }
-                } else if (c != '\r') {
-                    currentLine += c;
-                }
-            }
-        }
-
-        if (WiFi.getMode() == WIFI_STA) {
-            if (WiFi.status() == WL_CONNECTED) {
-                if (client.connected()) {
-                    // read sensor data
-                    DHT dht(DHT_PIN, DHT_TYPE);
-                    float h = dht.readHumidity();
-                    float t = dht.readTemperature();
-                    // check if any reads failed and exit early, then try again
-                    if (isnan(h) || isnan(t)) {
-                        Serial.println("Failed to read from DHT sensor!");
-                        return;
                     }
                 }
             }
         }
+    } else if (WiFi.getMode() == WIFI_STA) {
+        WiFi.begin(STA_SSID, STA_PW);
+        Serial.println("Connecting to WiFi");
 
-        header = "";
-        client.stop();
-        Serial.println("Client disconnected");
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(100);
+            Serial.print(".");
+        }
+
+        if (WiFi.status() == WL_CONNECTED) {
+            Serial.println("Connected to STA WiFi");
+            Serial.println("IP address: ");
+            Serial.println(WiFi.localIP());
+            if (client.connected()) {
+                float humidity = dht.readHumidity();
+                float temperature = dht.readTemperature();
+
+                if (isnan(humidity) || isnan(temperature)) {
+                    Serial.println("Failed to read from DHT sensor!");
+                    return;
+                }
+
+                // send temperature as json data to route /temperature
+                StaticJsonDocument<200> doc;
+                doc["temperature"] = temperature;
+                String output;
+                serializeJson(doc, output);
+                client.print("POST /temperature HTTP/1.1\r");
+
+                // send humidity as json data to route /humidity
+                StaticJsonDocument<200> doc2;
+                doc2["humidity"] = humidity;
+                String output2;
+                serializeJson(doc2, output2);
+                client.print("POST /humidity HTTP/1.1\r");
+            }
+        }
     }
 }
